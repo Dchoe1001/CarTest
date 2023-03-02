f7d2e63ee4c32040f95a1c4a5dc2ef02cad0ee2a
Dchoe1001 <121629758+Dchoe1001@users.noreply.github.com>
2023-03-02 07:48:49 -0500

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,12 +1,48 @@
 using UnityEngine;
+using static LogitechGSDK;
 
 public class test : MonoBehaviour
 {
-    private const string A_BUTTON = "A Button"; // G920 A BUTTON
+    private const string gas = "GasPedal"; // G920 A BUTTON
+    private const string pedal = "BrakePedal"; // G920 A BUTTON
+    private string actualState;
 
+    // Controller index (0 = first controller)
+    public int controllerIndex = 0;
     private void Update()
     {
-        float aButtonValue = Input.GetAxisRaw(A_BUTTON);
-        Debug.Log("A button value: " + aButtonValue);
+        float gasInput = Input.GetAxisRaw("GasPedal"); // Get raw input from the G920's gas pedal
+        float brakeInput = Input.GetAxisRaw("BrakePedal"); // Get raw input from the G920's brake pedal
+
+        Debug.Log("gas button value: " + gasInput);
+        //Debug.Log("brake button value: " + brakeInput);
+        
+        
+        
+        //LogitechGSDK.DIJOYSTATE2ENGINES previousState = new LogitechGSDK.DIJOYSTATE2ENGINES();
+        //LogitechGSDK.DIJOYSTATE2ENGINES currentState = LogitechGSDK.LogiGetStateUnity(0);
+
+        /*if (previousState.lX != currentState.lX || previousState.lY != currentState.lY || previousState.lZ != currentState.lZ)
+        {
+            actualState += "x-axis position :" + currentState.lX + "\n";
+        }
+
+        if (currentState != null)
+        {
+            // Access members of currentState here
+        }
+
+        previousState = currentState;
+
+
+        /*if (LogiUpdate() && LogiIsConnected(controllerIndex))
+        {
+            DIJOYSTATE2ENGINES state = LogiGetStateUnity(controllerIndex);
+
+            // Get X-axis rotation from the steering wheel
+            float xRotation = state.lRx;
+            Debug.Log("xRotation " + xRotation);
+
+        }*/
     }
 }
