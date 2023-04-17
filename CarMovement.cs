b3732285a9d7aff6ddceb40a6c1a1a7281891068
Dchoe1001 <121629758+Dchoe1001@users.noreply.github.com>
2023-04-17 18:04:13 -0400

    Update CarMovement.cs

--- CarMovement.cs
+++ CarMovement.cs
@@ -7,18 +7,26 @@ public class CarMovement : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     private float currentSteerAngle;
-    private float currentbreakForce;
-    private float isBreaking;
+    private float currentbreakForce; // clutch pedal brake
+    //private float currentbreakForce2; // brake pedal brake **doesnt work
+    private float isBreaking; // clutch pedal
+    //private float isBreaking2; //brake pedal
 
-    [SerializeField] private float motorForce = 10000f; // acceleration speed
-    [SerializeField] private float breakForce = 2000f;
-    [SerializeField] private float maxSteerAngle = 180f;
-    [SerializeField] private float minSteerAngle = 0f; 
-    [SerializeField] public float deadZone = 1f;  // The range of values around zero to ignore
-    [SerializeField] public float steeringSensitivity = 45f;
-    [SerializeField] public float accelerationFactor = 100f; 
+    private Rigidbody carRigidBody;
 
-    [SerializeField] public float breakDuration = 10f; 
+    [SerializeField] private float motorForce = 1000f; // acceleration speed
+    [SerializeField] private float breakForce = 75f;
+    [SerializeField] private float maxSteerAngle = 150f;
+    [SerializeField] private float minSteerAngle = 0f;
+    [SerializeField] public float deadZone = 10f;  // The range of values around zero to ignore
+    [SerializeField] public float steeringSensitivity = 5f;
+    [SerializeField] public float accelerationFactor = 100f;
+    [SerializeField] public float breakDuration = 2500f;
+    [SerializeField] public float accelDuration = 7500f;
+    [SerializeField] public float velocityCap = 10f;
+
+    [SerializeField] public float minrotationspd = 0f;
+    [SerializeField] public float maxrotationspd = 120f;
 
     [SerializeField] private WheelCollider frontLeftWheelCollider;
     [SerializeField] private WheelCollider frontRightWheelCollider;
@@ -30,14 +38,20 @@ public class CarMovement : MonoBehaviour
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
+
+    void Start()
+    {
+        carRigidBody = GetComponent<Rigidbody>();
+    }
+
     private void FixedUpdate()
     {
         GetInput();
         HandleMotor();
-
-       /* if (Mathf.Abs(horizontalInput) < deadZone) {
-            horizontalInput = 0f;  // Apply dead zone to the input
-        }*/
+        if (carRigidBody.velocity.magnitude > velocityCap)
+        {
+            carRigidBody.velocity = carRigidBody.velocity.normalized * velocityCap;
+        }
 
         HandleSteering();
         UpdateWheels();
@@ -46,70 +60,137 @@ public class CarMovement : MonoBehaviour
 
     private void GetInput()
     {
-        horizontalInput = Input.GetAxis("Steering");
+        if (Input.GetAxis("Steering") < 1f)
+        {
+            horizontalInput = 0f;
+        }
+        else
+        {
+            horizontalInput = Input.GetAxis("Steering");
+        }
+        Debug.Log("SSSS" + horizontalInput);
+
         verticalInput = Input.GetAxisRaw("GasPedal");
         isBreaking = Input.GetAxisRaw("BrakePedal");
-        //Debug.Log("Brake Pedal Input: " + isBreaking);
+        //isBreaking2 = Input.GetAxisRaw("BrakePedal2");
+        //Debug.Log(horizontalInput);
+
     }
 
 
     private void HandleMotor()
     {
-        //float isTerTrue = 1;
-        //float isTerFalse = 0;
-        if(verticalInput < 0f) {
+        float isTerTrue = 1;
+        float isTerFalse = 0;
+        if (verticalInput < 0f)
+        {
             frontLeftWheelCollider.motorTorque = -verticalInput * motorForce;
             frontRightWheelCollider.motorTorque = -verticalInput * motorForce;
         }
-        else {
+        else
+        {
             frontLeftWheelCollider.motorTorque = 0;
             frontRightWheelCollider.motorTorque = 0;
         }
         currentbreakForce = isBreaking != 1f ? breakForce : 0f; // Apply breaking only if isBreaking is not equal to 1
-        //currentbreakForce = isBreaking != 1f ? isTerTrue : isTerFalse;
+        currentbreakForce = isBreaking != 1f ? isTerTrue : isTerFalse;
         //Debug.Log("ANS: " + currentbreakForce);
-        ApplyBreaking();           
+        //currentbreakForce2 = isBreaking2 != 1f ? isTerTrue : isTerFalse;
+        //Debug.Log("Clutch pedal: " + currentbreakForce);
+        //Debug.Log("Brake pedal: " + currentbreakForce2);
+        ApplyBreaking();
     }
 
     private void ApplyBreaking()
-{
-    if (isBreaking != 1f) {
-        // Gradually increase the brake force up to the maximum over a certain duration
-        currentbreakForce = Mathf.MoveTowards(currentbreakForce, breakForce, Time.fixedDeltaTime * (breakForce / breakDuration));
+    {
+        if (isBreaking != 1f)
+        {
+            // Gradually increase the brake force up to the maximum over a certain duration
+            currentbreakForce = Mathf.MoveTowards(currentbreakForce, breakForce, Time.fixedDeltaTime * (breakForce / breakDuration));
 
-        // Gradually decrease the motor force down to zero over a certain duration
-        motorForce = Mathf.MoveTowards(motorForce, 0f, Time.fixedDeltaTime * (motorForce / breakDuration));
-    } else {
-        // Gradually decrease the brake force down to zero over a certain duration
-        currentbreakForce = Mathf.MoveTowards(currentbreakForce, 0f, Time.fixedDeltaTime * (breakForce / breakDuration));
+            // Gradually decrease the motor force down to zero over a certain duration
+            motorForce = Mathf.MoveTowards(motorForce, 0f, Time.fixedDeltaTime * (motorForce / accelDuration));
+        }
+        else
+        {
+            // Gradually decrease the brake force down to zero over a certain duration
+            currentbreakForce = Mathf.MoveTowards(currentbreakForce, 0f, Time.fixedDeltaTime * (breakForce / breakDuration));
 
-        // Gradually increase the motor force up to the maximum over a certain duration
-        motorForce = Mathf.MoveTowards(motorForce, 10000f, Time.fixedDeltaTime * (10000f / breakDuration));
-    }
-    
-    frontRightWheelCollider.brakeTorque = currentbreakForce;
-    frontLeftWheelCollider.brakeTorque = currentbreakForce;
-    rearLeftWheelCollider.brakeTorque = currentbreakForce;
-    rearRightWheelCollider.brakeTorque = currentbreakForce;
-}
+            // Gradually increase the motor force up to the maximum over a certain duration
+            motorForce = Mathf.MoveTowards(motorForce, 1000f, Time.fixedDeltaTime * (1000f / accelDuration));
+        }
 
-
-
-   private void HandleSteering()
-{
-    float rawInput = Input.GetAxisRaw("Steering"); // Get the raw input value from the G920 steering wheel
-    float input = Mathf.Clamp(rawInput, -1f, 1f); // Clamp the input value between -1 and 1
-
-    currentSteerAngle = Mathf.Lerp(minSteerAngle, maxSteerAngle, Mathf.Abs(input)) * Mathf.Sign(input);
-    Debug.Log("currentSteerAngle" + currentSteerAngle);
-
-    if (Mathf.Abs(currentSteerAngle) < deadZone) {
-        input = 0f;
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
     }
 
-    frontLeftWheelCollider.steerAngle = currentSteerAngle / steeringSensitivity;
-    frontRightWheelCollider.steerAngle = currentSteerAngle / steeringSensitivity;
-}
+
+
+    private void HandleSteering()
+    {
+        float rawInput = Input.GetAxisRaw("Steering"); // Get the raw input value from the G920 steering wheel
+        Debug.Log(rawInput);
+        float input = Mathf.Clamp(rawInput, -1f, 1f); // Clamp the input value between -1 and 1
+        
+
+
+        currentSteerAngle = Mathf.Lerp(minSteerAngle, maxSteerAngle, Mathf.Abs(input)) * Mathf.Sign(input);
+        //Debug.Log("currentSteerAngle" + currentSteerAngle);
+
+        if (Mathf.Abs(currentSteerAngle) < deadZone)
+        {
+            input = 0f;
+        }
+
+        frontLeftWheelCollider.steerAngle = currentSteerAngle / steeringSensitivity;
+        frontRightWheelCollider.steerAngle = currentSteerAngle / steeringSensitivity;
+
+        if (Mathf.Abs(verticalInput) < .1f)
+        {
+            currentSteerAngle = 0f;
+        }
+        else
+        {
+            float angle = 0;
+
+            // check if the input is above a certain threshold value
+            if (Mathf.Abs(input) > 0.1f)
+            {
+                angle = input / steeringSensitivity;
+
+                // gradually rotate the car toward the desired angle
+                float rotationSpeed = Mathf.Clamp(GetComponent<Rigidbody>().velocity.magnitude / 2, minrotationspd, maxrotationspd);
+                Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+                // update current steer angle based on input
+                currentSteerAngle = Mathf.Lerp(minSteerAngle, maxSteerAngle, Mathf.Abs(input)) * Mathf.Sign(input);
+            }
+            else
+            {
+                // if input is below the threshold value, set current steer angle to 0
+                currentSteerAngle = 0;
+            }
+
+        }
+        //Debug.Log("Steering " + currentSteerAngle);
+        if (Mathf.Abs(currentSteerAngle) < deadZone)
+        {
+            currentSteerAngle = 0f;
+            frontLeftWheelCollider.steerAngle = 0f;
+            frontRightWheelCollider.steerAngle = 0f;
+            //Debug.Log("*****DEADZONE*****-");
+        }
+        else
+        {
+            frontLeftWheelCollider.steerAngle = currentSteerAngle / steeringSensitivity;
+            frontRightWheelCollider.steerAngle = currentSteerAngle / steeringSensitivity;
+        }
+
+
+    }
 
 
 
@@ -125,9 +206,8 @@ public class CarMovement : MonoBehaviour
     {
         Vector3 pos;
         Quaternion rot;
-;       wheelCollider.GetWorldPose(out pos, out rot);
+        wheelCollider.GetWorldPose(out pos, out rot);
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
     }
 }
-
